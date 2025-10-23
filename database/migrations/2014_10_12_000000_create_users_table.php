9620f3e5daeba7d5bf4f6ee278c31c57e9d2f115
fallousenghor <fallousenghor@gmail.com>
2025-10-23 15:53:09 +0000

    Disable migrations transaction for Neon compatibility

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
+     * Disable wrapping this migration in a transaction (required for some Postgres operations on Neon).
+     */
+    public $withinTransaction = false;
+    /**
      * Run the migrations.
      */
     public function up(): void
